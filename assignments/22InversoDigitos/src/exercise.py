27d44b9904ad7a8ae2b042b6f35bb1d63784e0c6
luiscarlos55 <a01254044@itesm.mx>
2021-10-15 17:24:45 +0000

    EXAMEN PARTE PRACTICA 1

--- assignments/22InversoDigitos/src/exercise.py
+++ assignments/22InversoDigitos/src/exercise.py
@@ -1,8 +1,19 @@
 def main():
-    num = int(input("Enter a number: "))
+    number = int(input("Enter a number: "))
     #escribe tu código abajo de esta línea
+    numberstr= str(number)
+    if len(numberstr)>6:
+        print('Too long')
+    else:
+     if number >0:
+        reverso= numberstr[len(numberstr)::-1]
+        print(reverso)
+     else:
+        negativo= numberstr[0]
+        reverso= numberstr[len(numberstr):0:-1]
+        reversaneg= negativo + reverso
+        print(reversaneg)  
     
-
 
 if __name__=='__main__':
     main()
